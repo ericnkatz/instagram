29613400176f52abd0658c446d752e61ac49ad74
ericnkatz <admin@ericnkatz.com>
2015-03-13 11:12:30 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+require 'vendor/autoload.php';
+Dotenv::load(__DIR__);
+
+$app = new \Slim\Slim();
+
+$cache = new \Doctrine\Common\Cache\FilesystemCache('cache');
+
+
+$auth_config = array(
+	'client_id'         => getenv('INSTAGRAM_CLIENT_ID'),
+	'client_secret'     => getenv('INSTAGRAM_CLIENT_SECRET'),
+	'redirect_uri'      => getenv('INSTAGRAM_REDIRECT_URI'),
+	'scope'             => array( 'basic' )
+);
+
+$instagram = new \Instagram\Instagram;
+$instagram->setClientID( $auth_config['client_id'] );
+
+
+
+$app->get('/tag/:tag(/:page)', function ($tag, $page = null) use ($app, $instagram, $cache) {
+	$term = 'unique_' . $tag . $page;
+	$params = isset($page) ? array( 'max_id' => $page ) : null;
+	$now = time();
+	// $app->etag($term);
+	// $app->expires('+5 seconds');
+
+    $storage = $cache->fetch($term);
+    if($storage['expiration'] < $now) {
+
+	    $data = function() use($instagram, $tag, $params) {
+	    	$tag = $instagram->getTag($tag)->getMedia( $params );
+			$images = [];
+			foreach($tag as $image) {
+				$images[] = [
+					'id' => $image->id,
+					'link' => $image->link,
+					'image' => $image->images->standard_resolution->url,
+					'author' => [
+						'username' => $image->user->username,
+						'full_name' => $image->user->full_name
+					]
+				];
+			}
+	    return [$images, $tag];
+	    };
+
+	    $data = $data();
+
+	    $storage = [
+	    	'images' => $data[0],
+	    	'next' => 'http://' . $_SERVER['HTTP_HOST'] . '/tag/' . $tag . '/' . $data[1]->getNext(),
+	    	'expiration' => strtotime('+5 seconds')
+	    ];
+	    $cache->save($term, $storage);
+	}
+
+	echo json_encode($storage);
+
+});
+
+
+
+
+
+$app->run();
